aee1ed8f0d3aed5130c8817164e1f4795d0bd57b
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-22 17:31:00 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book 
+<<<<<<< HEAD
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   def initialize(title)
@@ -11,5 +12,14 @@ end
 
 one = Book.new("And Then There Were None")
 one.page_count = 272
+=======
+  def initialize(title)
+    @title = title 
+  end 
+  def title 
+    @title 
+  end 
+end 
+>>>>>>> b234daa320aed8c068a0a11356494314ec3cc694
 
-
+Book.new("And Then There Were None")
